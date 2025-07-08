30f07afd325a14e55e8066972ded06154c1affeb
carolineeeesouzaaaa <125361828+carolineeeesouzaaaa@users.noreply.github.com>
2025-07-08 09:07:38 -0300

    aula 5

---
+++ main.js
@@ -0,0 +1,30 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.paramentro-senha__botao');
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+campoSenha.value = letrasMaiusculas;
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+   // tamanhoSenha = tamanhoSenha - 1;
+   tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+    // tamanhoSenha = tamanhoSenha + 1;
+    tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
